f8732f62a2032d5e9b201a642b889724af8c099f
Amir Khatiby <amirhif123@hotmail.com>
2023-10-18 19:46:59 +0200

    Added Java backend

---
+++ absurdbets/src/main/java/com/absurdbetscorp/absurdbets/AbsurdbetsApplication.java
@@ -0,0 +1,13 @@
+package com.absurdbetscorp.absurdbets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsurdbetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsurdbetsApplication.class, args);
+	}
+
+}
---
+++ absurdbets/src/test/java/com/absurdbetscorp/absurdbets/AbsurdbetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absurdbetscorp.absurdbets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsurdbetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
