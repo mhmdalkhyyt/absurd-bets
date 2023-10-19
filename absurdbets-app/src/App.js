bff858be60c43c032de8f5fcbd5b207b25f99935
mhmdalkhyyt <mhmdalkhyyt@gmail.com>
2023-10-19 19:44:56 +0200

    added fetch arg

--- absurdbets-app/src/App.js
+++ absurdbets-app/src/App.js
@@ -90,7 +90,7 @@ const ConnectionStatus = () => {
 
   useEffect(() => {
     // Fetch connection status from the backend when the component mounts
-    fetch('/api/connection-status') // Replace with your actual API endpoint
+    fetch('jdbc:mysql://localhost:3306/AbsurdBetsTestDB') // Replace with your actual API endpoint
       .then(response => response.json())
       .then(data => setStatus(data.status))
       .catch(error => {
