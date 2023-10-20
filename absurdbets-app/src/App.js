d0a9b32371911d63143664609a47868e2d2c0581
Amir Khatiby <amirhif123@hotmail.com>
2023-10-20 19:54:50 +0200

    update

--- absurdbets-app/src/App.js
+++ absurdbets-app/src/App.js
@@ -21,9 +21,9 @@ const LandingPage = () => {
 
         <div>
           <div>
-            <PageTable/>
+            <List></List>
           </div>
-        <GetBet/>
+
         </div>
       
       </section>
@@ -150,6 +150,24 @@ const PageTable = () => {
   );
 
 };
+
+export default function List() {
+    const listItems = people.map(person =>
+        <li key={person.id}>
+            <img
+                src={getImageUrl(person)}
+                alt={person.name}
+            />
+            <p>
+                <b>{person.name}</b>
+                {' ' + person.profession + ' '}
+                known for {person.accomplishment}
+            </p>
+        </li>
+    );
+    return <ul>{listItems}</ul>;
+}
+
 
 
 
